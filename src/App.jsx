d274719ed1bcd6d67ee9133bf10cd7b375f29d9f
Dmitry Rakovich <dmitryrakovich@mail.ru>
2023-06-25 14:42:38 +0300

    feat: add state, refactor component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import { useState } from 'react'
+import Header from './components/Header'
+import Products from './components/Products'
+
 function App() {
+  const [category, setCategory] = useState('all')
+  const [currency, setCurrency] = useState('USD')
   return (
     <>
-      <h1>Hello</h1>
+      <Header {...{setCategory, category, setCurrency, currency}}/>
+      <Products {...{category, currency}}/>
     </>
   )
 }
